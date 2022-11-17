36293528f266b492d2dec75ad187fbbd3d9150c4
sleepyshowdy <hojo.miku@gmail.com>
2022-11-18 07:08:27 +0900

    マジックパレット追加

--- scripts/js_1.js
+++ scripts/js_1.js
@@ -61,3 +61,16 @@ btnColor.addEventListener("click", ()=>{
  btnRemove.addEventListener("click",()=>{
     controller.abort(); // removes any/all event handlers associated with this controller
  })
+
+ // マジックパレット
+ function random(number) {
+    return Math.floor(Math.random()*number);
+  }
+
+  function bgChange() {
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    return rndCol;
+  }
+
+  const container = document.querySelector('#container');
+  container.addEventListener('click', (event) => event.target.style.backgroundColor = bgChange());
